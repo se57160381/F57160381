470be6694bc98ccbdb422f9984de751e47a1ae82
BassAye <se57160381@gmail.com>
2017-05-03 18:28:11 +0700

    Initial commit

---
+++ app/src/main/java/com/bas/testlogin/MainActivity.java
@@ -0,0 +1,60 @@
+package com.bas.testlogin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    TextView txtLoginStatus;
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        loginButton = (LoginButton) findViewById(R.id.fb_login_bn);
+        txtLoginStatus = (TextView)findViewById(R.id.txtLoginStatus);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                txtLoginStatus.setText("Login Success \n" +
+                        loginResult.getAccessToken().getUserId()+
+                        "\n" + loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                txtLoginStatus.setText("Login Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+    }
+}
+
+
+
+
+
